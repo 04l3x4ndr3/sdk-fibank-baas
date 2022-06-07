723dc4660acfafec4a87bc4476008c345a967c33
04l3x4ndr3 <alexandre@2plug.com.br>
2022-06-07 10:33:23 -0300

    issue account class

--- src/OnBoarding/Account.php
+++ src/OnBoarding/Account.php
@@ -127,7 +127,7 @@ class Account extends AccountHolder
             "BankBranch" => $bankBranch ?? "",
             "BankAccount" => $bankAccount ?? "",
             "BankAccountDigit" => $bankAccountDigit ?? "",
-            "OnlyBalance" => $onlyBalance,
+            "OnlyBalance" => ($onlyBalance) ? "true" : "false",
             "EntryClassificationType" => $entryClassificationType
         ];
         $call = $http->call('GetAccountEntry', $data);

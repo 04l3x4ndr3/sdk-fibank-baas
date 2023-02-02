fe563d0c0c8479854340830ea4f3a28e41d23545
04l3x4ndr3 <alexandre@2plug.com.br>
2023-02-02 11:24:42 -0300

    Atualização de retorno do método getDocument, de array para Object Document

--- src/OnBoarding/Documents.php
+++ src/OnBoarding/Documents.php
@@ -3,9 +3,11 @@
 namespace O4l3x4ndr3\SdkFitbank\OnBoarding;
 
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Exception\InvalidArgumentException;
 use O4l3x4ndr3\SdkFitbank\Common\Document;
 use O4l3x4ndr3\SdkFitbank\Configuration;
 use O4l3x4ndr3\SdkFitbank\Helpers\CallApi;
+use O4l3x4ndr3\SdkFitbank\Helpers\ListValues;
 
 class Documents extends CallApi
 {
@@ -24,7 +26,19 @@ class Documents extends CallApi
      */
     public function getDocument(string $taxNumber, int $documentType): object
     {
-        return $this->call('GetDocument', array_filter(['TaxNumber' => $taxNumber, 'DocumentType' => $documentType])
+        $resp = $this->call('GetDocument', array_filter(['TaxNumber' => $taxNumber, 'DocumentType' => $documentType]));
+
+        if ($resp->Success !== "true") {
+            throw new InvalidArgumentException($resp->Message, 200);
+        }
+
+        return new Document(
+            $resp->Document,
+            ListValues::getDocumentFormatIdByValue($resp->Extension),
+            $resp->FileName,
+            $documentType,
+            $resp->Description,
+            $resp->ExpirationDate
         );
     }
 

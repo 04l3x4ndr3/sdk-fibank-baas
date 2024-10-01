1e12ebb7de7fb0743a9c69cd69de2d715a5b0c50
04l3x4ndr3 <alexandre@2plug.com.br>
2024-10-01 18:12:28 -0300

    Add getter and setter methods for DARF properties
    Introduced getter and setter methods for multiple DARF properties including tax number, bank details, DARF type, calculation period, gross revenue, fines, interest, payment dates, tags, and more. These additions enhance the encapsulation and manipulation of DARF object attributes, providing a more robust and flexible API.

--- src/Payments/DARF.php
+++ src/Payments/DARF.php
@@ -77,6 +77,384 @@ class DARF
     }
 
     /**
+     * @return string|null
+     */
+    public function getTaxNumber(): ?string
+    {
+        return $this->taxNumber;
+    }
+
+    /**
+     * @param string|null $taxNumber
+     * @return DARF
+     */
+    public function setTaxNumber(?string $taxNumber): DARF
+    {
+        $this->taxNumber = $taxNumber;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getFromBank(): ?int
+    {
+        return $this->fromBank;
+    }
+
+    /**
+     * @param int|null $fromBank
+     * @return DARF
+     */
+    public function setFromBank(?int $fromBank): DARF
+    {
+        $this->fromBank = $fromBank;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFromBankBranch(): ?string
+    {
+        return $this->fromBankBranch;
+    }
+
+    /**
+     * @param string|null $fromBankBranch
+     * @return DARF
+     */
+    public function setFromBankBranch(?string $fromBankBranch): DARF
+    {
+        $this->fromBankBranch = $fromBankBranch;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFromBankAccount(): ?string
+    {
+        return $this->fromBankAccount;
+    }
+
+    /**
+     * @param string|null $fromBankAccount
+     * @return DARF
+     */
+    public function setFromBankAccount(?string $fromBankAccount): DARF
+    {
+        $this->fromBankAccount = $fromBankAccount;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFromBankAccountDigit(): ?string
+    {
+        return $this->fromBankAccountDigit;
+    }
+
+    /**
+     * @param string|null $fromBankAccountDigit
+     * @return DARF
+     */
+    public function setFromBankAccountDigit(?string $fromBankAccountDigit): DARF
+    {
+        $this->fromBankAccountDigit = $fromBankAccountDigit;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDARFType(): ?int
+    {
+        return $this->DARFType;
+    }
+
+    /**
+     * @param int|null $DARFType
+     * @return DARF
+     */
+    public function setDARFType(?int $DARFType): DARF
+    {
+        $this->DARFType = $DARFType;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getCalculationPeriod(): ?string
+    {
+        return $this->calculationPeriod;
+    }
+
+    /**
+     * @param string|null $calculationPeriod
+     * @return DARF
+     */
+    public function setCalculationPeriod(?string $calculationPeriod): DARF
+    {
+        $this->calculationPeriod = $calculationPeriod;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getCodeRevenue(): ?int
+    {
+        return $this->codeRevenue;
+    }
+
+    /**
+     * @param int|null $codeRevenue
+     * @return DARF
+     */
+    public function setCodeRevenue(?int $codeRevenue): DARF
+    {
+        $this->codeRevenue = $codeRevenue;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getContributorTaxNumber(): ?string
+    {
+        return $this->contributorTaxNumber;
+    }
+
+    /**
+     * @param string|null $contributorTaxNumber
+     * @return DARF
+     */
+    public function setContributorTaxNumber(?string $contributorTaxNumber): DARF
+    {
+        $this->contributorTaxNumber = $contributorTaxNumber;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getReferenceNumber(): ?string
+    {
+        return $this->referenceNumber;
+    }
+
+    /**
+     * @param string|null $referenceNumber
+     * @return DARF
+     */
+    public function setReferenceNumber(?string $referenceNumber): DARF
+    {
+        $this->referenceNumber = $referenceNumber;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDueDate(): ?string
+    {
+        return $this->dueDate;
+    }
+
+    /**
+     * @param string|null $dueDate
+     * @return DARF
+     */
+    public function setDueDate(?string $dueDate): DARF
+    {
+        $this->dueDate = $dueDate;
+        return $this;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getGrossRevenueValue(): ?float
+    {
+        return $this->grossRevenueValue;
+    }
+
+    /**
+     * @param float|null $grossRevenueValue
+     * @return DARF
+     */
+    public function setGrossRevenueValue(?float $grossRevenueValue): DARF
+    {
+        $this->grossRevenueValue = $grossRevenueValue;
+        return $this;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getGrossRevenuePercentage(): ?float
+    {
+        return $this->grossRevenuePercentage;
+    }
+
+    /**
+     * @param float|null $grossRevenuePercentage
+     * @return DARF
+     */
+    public function setGrossRevenuePercentage(?float $grossRevenuePercentage): DARF
+    {
+        $this->grossRevenuePercentage = $grossRevenuePercentage;
+        return $this;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getPrincipalValue(): ?float
+    {
+        return $this->principalValue;
+    }
+
+    /**
+     * @param float|null $principalValue
+     * @return DARF
+     */
+    public function setPrincipalValue(?float $principalValue): DARF
+    {
+        $this->principalValue = $principalValue;
+        return $this;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getFineValue(): ?float
+    {
+        return $this->fineValue;
+    }
+
+    /**
+     * @param float|null $fineValue
+     * @return DARF
+     */
+    public function setFineValue(?float $fineValue): DARF
+    {
+        $this->fineValue = $fineValue;
+        return $this;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getInterestValue(): ?float
+    {
+        return $this->interestValue;
+    }
+
+    /**
+     * @param float|null $interestValue
+     * @return DARF
+     */
+    public function setInterestValue(?float $interestValue): DARF
+    {
+        $this->interestValue = $interestValue;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPaymentDate(): ?string
+    {
+        return $this->paymentDate;
+    }
+
+    /**
+     * @param string|null $paymentDate
+     * @return DARF
+     */
+    public function setPaymentDate(?string $paymentDate): DARF
+    {
+        $this->paymentDate = $paymentDate;
+        return $this;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getTags(): ?array
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param array|null $tags
+     * @return DARF
+     */
+    public function setTags(?array $tags): DARF
+    {
+        $this->tags = $tags;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     * @return DARF
+     */
+    public function setDescription(?string $description): DARF
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIdentifier(): ?string
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * @param string|null $identifier
+     * @return DARF
+     */
+    public function setIdentifier(?string $identifier): DARF
+    {
+        $this->identifier = $identifier;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getRateValueType(): ?int
+    {
+        return $this->rateValueType;
+    }
+
+    /**
+     * @param int|null $rateValueType
+     * @return DARF
+     */
+    public function setRateValueType(?int $rateValueType): DARF
+    {
+        $this->rateValueType = $rateValueType;
+        return $this;
+    }
+
+    /**
      * @description Generates a DARF payment.
      * @document https://dev.fitbank.com.br/reference/26-1
      * @return object
